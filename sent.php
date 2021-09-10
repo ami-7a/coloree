20231133d94c90a71de29ae125be6914eeb0edf6
Amy K <ami7a121@gmail.com>
2021-09-10 10:00:31 +0900

    php

--- sent.php
+++ sent.php
@@ -6,8 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Colorful｜お問い合わせありがとうございます</title>
     <link rel="stylesheet" href="css/ress.css">
+    <link rel="stylesheet" href="css/sent.css">
     <link rel="stylesheet" href="css/common.css">
-    <link rel="stylesheet" href="./css/sent.css">
+    
     <link rel="shortcut icon" href="img/logo.ico" type="image/x-icon">
     <meta name="description" content="素敵な出会いのお手伝いを。福岡の結婚相談所、カラフルです。オンライン婚活、各種イベント、カウンセラー相談により、安心、楽しい婚活を！お気軽にご相談ください">
 
